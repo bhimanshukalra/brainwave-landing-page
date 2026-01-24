daa1634d09939f01a379e46263a3fc856358f4ac
bhimanshukalra <bhimanshukalra@gmail.com>
2026-01-24 19:33:05 +0530

    feat: updated header to cater to mobile view

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,14 @@
 import type { ReactNode } from "react";
 import ButtonSvg from "../assets/svg/ButtonSvg";
+import { twMerge } from "tailwind-merge";
 
 interface ButtonProps {
   className: string;
-  href: string;
+  href?: string;
   children?: ReactNode;
   px?: string;
   white?: boolean;
+  onClick?: () => void;
 }
 
 export const Button = ({
@@ -15,20 +17,26 @@ export const Button = ({
   href,
   px,
   white,
+  onClick,
 }: ButtonProps) => {
-  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || "px-7"} ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
+  const classes = twMerge(
+    "button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1",
+    px || "px-7",
+    white ? "text-n-8" : "text-n-1",
+    className,
+  );
 
   const spanClasses = "relative z-10";
 
   const renderButton = () => (
-    <button className={classes}>
+    <button className={classes} onClick={onClick}>
       <span className={spanClasses}>{children}</span>
       <ButtonSvg white={white} />
     </button>
   );
 
   const renderLink = () => (
-    <a href={href} className={classes}>
+    <a href={href} className={classes} onClick={onClick}>
       <span className={spanClasses}>{children}</span>
       <ButtonSvg white={white} />
     </a>
