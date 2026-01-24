df3a9dfa5e0ddc71cc5353cbd6bdc20f7011c058
bhimanshukalra <bhimanshukalra@gmail.com>
2026-01-24 18:48:30 +0530

    feat: implement header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import { Button } from "./components";
+import { Header } from "./components";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
       <div className="pt-20 lg:pt-20 overflow-hidden">
-        <Button className="mt-10" href="#login">
-          Something
-        </Button>
+        <Header />
       </div>
       <ButtonGradient />
     </>
